89b378adbdc1547962ccb5bc6c15459c877c7003
Packmanager9 <36295363+Packmanager9@users.noreply.github.com>
2020-09-20 19:24:00 -0700

    clean pound graphic

--- pohmao.js
+++ pohmao.js
@@ -1280,10 +1280,12 @@ window.addEventListener('DOMContentLoaded', (event) =>{
 
                 // if(pomao.body.isPointInside(pomao.tongue)){
                     if(this.width >= 50){
-                        pomao.body.xmom = -3*(pomao.dir)
-                        pomao.disabled = 1
-                        pomao.hits--
-                         pomao.body.ymom = -1.5
+                        if(pomao.disabled != 1){
+                            pomao.body.xmom = -3*(pomao.dir)
+                            pomao.disabled = 1
+                            pomao.hits--
+                             pomao.body.ymom = -1.5
+                        }
                     }
                 // }
             }
