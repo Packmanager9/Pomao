2257bc45b49a3fd8fa4f04fe36e59e4238542cb9
Packmanager9 <36295363+Packmanager9@users.noreply.github.com>
2021-10-06 15:10:56 -0700

    Add files via upload

--- pohmao.js
+++ pohmao.js
@@ -19242,6 +19242,44 @@ window.addEventListener('DOMContentLoaded', (event) => {
         }
     }
 
+
+    function getRandomColor() { // random color
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[(Math.floor(Math.random() * 16) + 0)];
+        }
+        return color;
+    }
+
+    function invertColor(hex) {
+        if (typeof hex == "undefined") {
+            return getRandomColor()
+        }
+        if (hex.indexOf('#') === 0) {
+            hex = hex.slice(1);
+        }
+        // convert 3-digit hex to 6-digits.
+        if (hex.length === 3 || hex.length === 4) {
+            hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+        }
+        if (hex.length !== 6 && hex.length != 8) {
+            return getRandomColor()
+            throw new Error('Invalid HEX color.');
+        }
+        // invert color components
+        var r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16),
+            g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16),
+            b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
+        // pad each with zeros and return
+        return '#' + padZero(r) + padZero(g) + padZero(b);
+    }
+    function padZero(str, len) {
+        len = len || 2;
+        var zeros = new Array(len).join('0');
+        return (zeros + str).slice(-len);
+    }
+
     
     class Scuttlefly{
         constructor(center){
@@ -19504,6 +19542,9 @@ window.addEventListener('DOMContentLoaded', (event) => {
                         this.edges[e].ymom *= 1.2
                         this.edges[e].xmom += (Math.random()-.5)*2
                         this.edges[e].ymom += (Math.random()-.5)*2
+                        if(Math.random()<.2){
+                            this.edges[e].color = invertColor(this.edges[e].color)
+                        }
                     }
                 }
             }
@@ -19564,7 +19605,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
             let roty = this.runner
             let cloroholder = getRandomDarkColor()
 
-            if(Math.random() < .8){
+            if(Math.random() < .7){ //.8
 
                 cloroholder = 'black'
             }
